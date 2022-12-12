f2128ff9f6996d539484c85e3c2a307c0cbb41be
Ahmad Al-harbi <alharbiahmed21@gmail.com>
2022-12-12 22:34:39 +0300

    'tt'

--- application.py
+++ application.py
@@ -1,9 +1,11 @@
-from flask import Flask
-from views import view
+from flask import Flask, render_template, request, redirect, url_for 
+#from views import view
 
 application = Flask(__name__)
-application.config['SECRET_KEY'] = 'gfsgjkdjnkdjd fjskfgnjksngjlf'
-application.register_blueprint(view, url_prefix="/")
+#application.config['SECRET_KEY'] = 'gfsgjkdjnkdjd fjskfgnjksngjlf'
+#application.register_blueprint(view, url_prefix="/")
 
-
+@application.route("/")
+def home():
+    return render_template("Main.HTML", name="Ahmad")
 
