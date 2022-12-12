878e58794a2c5c321bef5c90e6cd9d814d8552df
Ahmad Al-harbi <alharbiahmed21@gmail.com>
2022-12-12 22:13:16 +0300

    ‘Small’

--- application.py
+++ application.py
@@ -1,8 +1,10 @@
 from flask import Flask
 
 application = Flask(__name__)
+application.config['SECRET_KEY'] = 'gfsgjkdjnkdjd fjskfgnjksngjlf'
 
 @application.route('/')
 
 def hllow():
     return 'High'
+
