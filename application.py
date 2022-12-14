de618121384f071d17cc95d115e12840a5368d9b
Ahmad Al-harbi <alharbiahmed21@gmail.com>
2022-12-14 22:10:39 +0300

    'tt'

--- application.py
+++ application.py
@@ -1,11 +1,29 @@
-from flask import Flask, render_template, request, redirect, url_for 
-#from views import view
+
+import os
+from flask import Flask,redirect
 
 application = Flask(__name__)
+
+@application.route('/')
+def hello():
+    return redirect("http://www.google.com", code=302)
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    application.run(host='0.0.0.0', port=port)
+
+
+
+#from flask import Flask, render_template, request, redirect, url_for 
+#from views import view
+
+#application = Flask(__name__)
 #application.config['SECRET_KEY'] = 'gfsgjkdjnkdjd fjskfgnjksngjlf'
 #application.register_blueprint(view, url_prefix="/")
 
-@application.route("/")
-def home():
-    return render_template("Main.HTML", name="Ahmad")
+#@application.route("/")
+#def home():
+    #return render_template("Main.HTML", name="Ahmad")
     #return 'hhhhh'
+    #return redirect()
